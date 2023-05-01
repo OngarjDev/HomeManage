f53de31a24c6e4667c19f00d51c8ba07ffb7bb46
Ongarj <ongarj2314@gmail.com>
2023-05-01 16:10:24 +0700

    ปรับให้ Responsive เรียบร้อยแล้ว

---
+++ wwwroot/js/CheckSizeScreen.js
@@ -0,0 +1,7 @@
+﻿function WindowWidth() {
+    return window.innerWidth;
+}
+
+window.addEventListener('resize', function () {
+    DotNet.invokeMethodAsync("InsureManage", "OnWindowResize",WindowWidth())
+});
